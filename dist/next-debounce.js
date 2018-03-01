f7dcb3ada8b7772e3bcc10e05f8a3c013b1bda32
afeiship <1290657123@qq.com>
2018-03-01 23:04:03 +0800

    issue: omptizp

---
+++ dist/next-debounce.js
@@ -0,0 +1,25 @@
+(function () {
+
+  var global = global || this || self || window;
+  var nx = global.nx || require('next-js-core2');
+
+  nx.debounce = function(inCallback, inDelay,inContext) {
+    var timer = null;
+    var delay = inDelay || 100;
+    return function () {
+      var args = arguments;
+      var context = inContext || this;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        inCallback.apply(context, args);
+      }, inDelay);
+    };
+  };
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.debounce;
+  }
+
+
+}());
